91c6b06a824b3e449dee17e203c761ded3569e58
wjf <1907305038@qq.com>
2026-04-29 17:43:12 +0800

    添加了utils.js文件

---
+++ utils.js
@@ -0,0 +1,8 @@
+// 算法导航工具函数
+export function quickSort(arr) {
+    if (arr.length <= 1) return arr;
+    const pivot = arr[Math.floor(arr.length / 2)];
+    const left = arr.filter(x => x < pivot);
+    const right = arr.filter(x => x > pivot);
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
